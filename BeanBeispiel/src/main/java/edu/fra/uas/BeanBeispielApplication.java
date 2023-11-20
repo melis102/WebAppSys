82ca76e6fc690618f4e889735bfa43780948d727
melis102 <132212656+melis102@users.noreply.github.com>
2023-11-20 11:34:19 +0100

    update testkommetar

--- BeanBeispiel/src/main/java/edu/fra/uas/BeanBeispielApplication.java
+++ BeanBeispiel/src/main/java/edu/fra/uas/BeanBeispielApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+//testkommentar
 @SpringBootApplication
 public class BeanBeispielApplication {
 
